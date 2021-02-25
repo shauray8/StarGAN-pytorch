f0a3b0e3975bac4cd5d13727db4c51cb67abb341
Shauray <shauray9@gmail.com>
2021-02-25 23:47:13 +0530

    working on the training loop arranging the data is tough

--- StarGAN/train.py
+++ StarGAN/train.py
@@ -2,12 +2,19 @@ import torch
 from tqdm import trange
 from model import Generator, Discriminator
 import torch.optim as optim
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Dataset
+import itertools
 
 # training loop goes here
 
 input_nc = 3
 output_nc = 3
 lr = 0.0002
+batch_size = 32
+size=256
+dataset = "CelebA"
+selected_attrs = ['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Male', 'Young']
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -24,4 +31,21 @@ def number_of_paras(model):
 
     print(model,num)
 
+def onehot_labels(c_org, input_nc, dataset, selected_attrs):
+    pass    
 
+#Loading data
+transform = [   transforms.RandomCrop(size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((.5, .5, .5), (.5, .5, .5)) ]
+
+data_loader = DataLoader(datasets.ImageFolder("../../data/CelebA", transform=transform),
+        batch_size=batch_size, shuffle=True)
+
+data_iter = iter(data_loader)
+x_fixed, c_org = next(data_iter)
+#x_fixed = x_fixed.to(device)
+#c_fixed_list = onehot_labels(c_org, input_nc, dataset, selected_attrs)
+
+print(x_fixed[0])
