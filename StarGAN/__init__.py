f5179cce6b01b95ca67ae2ad122154ec678bdae6
Shauray <shauray9@gmail.com>
2021-03-26 23:02:11 +0530

    CUDA out of memory (sad)

--- StarGAN/__init__.py
+++ StarGAN/__init__.py
@@ -1,2 +1,7 @@
-import torch 
+from train import *
+from model import *
+from utils import *
 
+
+if __name__ == "__main__":
+    print("importing stuff for future use !")
--- StarGAN/test.py
+++ StarGAN/test.py
@@ -1,3 +1,5 @@
 import torch
 
 # testing of the model happens here 
+
+print("testing once the model is trained")
